0ca62b80dad35c636bff132c4f2683cdf3e42837
Kelsey Oshiro <kelseymosh@gmail.com>
2020-06-23 23:30:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,7 +21,6 @@ def using_flatten(instruments)
 end
 
 def using_delete(array, string)
-  string = "Steven"
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
   instructors.delete("Steven")
   puts instructors
